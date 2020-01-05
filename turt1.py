d261710cf3f926f06c081ac834e1a3b51a1ffc66
zahera-fatima <55752747+zahera-fatima@users.noreply.github.com>
2020-01-05 22:58:21 +0500

    Add files via upload

---
+++ turt1.py
@@ -0,0 +1,15 @@
+import turtle
+
+# turtle.shape("turtle")
+
+# turtle.forward(25)
+
+# turtle.exitonclick()
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
